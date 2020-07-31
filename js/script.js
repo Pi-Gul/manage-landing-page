12ad057b857e5721efef941384df6a10203b8a12
Pi-Gul <photon-drive@protonmail.com>
2020-07-31 19:29:09 +0200

    first mobile version; added a slider; style changes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let h1 = document.querySelector('h1');
+let menuIcon = document.querySelector('.menu-icon');
+
+const iconOpen = './images/icon-hamburger.svg'
+const iconClose = './images/icon-close.svg'
+
+h1.innerHTML = 'IT WORKS';
+
+console.log(menuIcon);
+
+function changeIcon() {
+  menuIconSrc = menuIcon.getAttribute('src');
+  console.log(menuIconSrc);
+  if(menuIconSrc===iconOpen) {
+    menuIcon.setAttribute('src', iconClose);
+  } else if(menuIconSrc===iconClose) {
+    menuIcon.setAttribute('src', iconOpen);
+  }
+}
+
+menuIcon.addEventListener('click', changeIcon);
